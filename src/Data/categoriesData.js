c35664b87856fe0386080ddd5837791412b31352
ortima <account.net@mail.ru>
2024-03-17 02:03:03 +0300

    fixes scr for gh pages

--- src/Data/categoriesData.js
+++ src/Data/categoriesData.js
@@ -1,15 +1,15 @@
 const categories = [
-    { src: 'src/assets/category/category-1.jpg', text: 'Еда для кошек' },
-    {
-        src: 'src/assets/category/category-2.jpg',
-        text: 'Игрушки для кошек',
-    },
-    { src: 'src/assets/category/category-3.jpg', text: 'Еда для собак' },
-    {
-        src: 'src/assets/category/category-4.jpg',
-        text: 'Игрушки для собак',
-    },
-    { src: 'src/assets/category/category-5.jpg', text: 'Рыбки' },
+  { src: '/src/assets/category/category-1.jpg', text: 'Еда для кошек' },
+  {
+    src: '/src/assets/category/category-2.jpg',
+    text: 'Игрушки для кошек',
+  },
+  { src: '/src/assets/category/category-3.jpg', text: 'Еда для собак' },
+  {
+    src: '/src/assets/category/category-4.jpg',
+    text: 'Игрушки для собак',
+  },
+  { src: '/src/assets/category/category-5.jpg', text: 'Рыбки' },
 ]
 
 export default categories
