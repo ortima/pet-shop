1b52f8733b1f50810f8859484626c0e7f36c0d54
ortima <account.net@mail.ru>
2024-03-17 14:03:11 +0300

    new structure

--- src/Data/advantagesData.js
+++ src/Data/advantagesData.js
@@ -1,33 +1,33 @@
 const advantages = [
-    {
-        id: 1,
-        src: 'src/assets/advantages/bonus-program.svg',
-        title: 'Бонусная программа',
-        description: 'Кэшбэк до 7% на бонусную карту',
-    },
-    {
-        id: 2,
-        src: 'src/assets/advantages/delivery.svg',
-        title: 'Быстрая доставка',
-        description: 'При оформлении заказа с 12:00 до 18:00',
-    },
-    {
-        id: 3,
-        src: 'src/assets/advantages/app.svg',
-        title: 'Удобное приложение',
-        description: 'Заказывайте через мобильное приложение',
-    },
-    {
-        id: 4,
-        src: 'src/assets/advantages/grooming.svg',
-        title: 'Груминг салоны',
-        description: 'Придадим форму вашему другу',
-    },
-    {
-        id: 5,
-        src: 'src/assets/advantages/pickup.svg',
-        title: 'Самовывоз заказа',
-        description: 'Через 30 мин из магазина',
-    },
+  {
+    id: 1,
+    src: '/advantages/bonus-program.svg',
+    title: 'Бонусная программа',
+    description: 'Кэшбэк до 7% на бонусную карту',
+  },
+  {
+    id: 2,
+    src: '/advantages/delivery.svg',
+    title: 'Быстрая доставка',
+    description: 'При оформлении заказа с 12:00 до 18:00',
+  },
+  {
+    id: 3,
+    src: '/advantages/app.svg',
+    title: 'Удобное приложение',
+    description: 'Заказывайте через мобильное приложение',
+  },
+  {
+    id: 4,
+    src: '/advantages/grooming.svg',
+    title: 'Груминг салоны',
+    description: 'Придадим форму вашему другу',
+  },
+  {
+    id: 5,
+    src: '/advantages/pickup.svg',
+    title: 'Самовывоз заказа',
+    description: 'Через 30 мин из магазина',
+  },
 ]
 export default advantages
--- src/components/FAQ/FAQList.jsx
+++ src/components/FAQ/FAQList.jsx
@@ -3,63 +3,59 @@ import styles from './FAQ.module.scss'
 import faqData from '../../Data/faqData'
 
 const FAQList = () => {
-    const [isOpen, setIsOpen] = useState({})
+  const [isOpen, setIsOpen] = useState({})
 
-    const toggleAnswer = (index) => {
-        setIsOpen((prevState) => ({
-            ...prevState,
-            [index]: !prevState[index],
-        }))
-    }
+  const toggleAnswer = (index) => {
+    setIsOpen((prevState) => ({
+      ...prevState,
+      [index]: !prevState[index],
+    }))
+  }
 
-    return (
-        <section className={styles.container}>
-            <img src="src\assets\logo.png" alt="logo" />
-            <div className={styles.content}>
-                <h1>
-                    Почему выбирают зоомагазин <span>«Природа» </span>?
-                </h1>
-                <p className={styles.subtitle}>
-                    «Природа» – крупнейшая сеть зоомагазинов и интернет-магазин
-                    с доставкой по всей России. На сайте priroda-zoo.ru
-                    представлен широкий выбор зоотоваров, акции и бонусы, а
-                    также сервисы по подбору кормов и полезные статьи о домашних
-                    животных. Есть приложение для iOS и Android.
-                </p>
+  return (
+    <section className={styles.container}>
+      <img src="/logo.png" alt="logo" />
+      <div className={styles.content}>
+        <h1>
+          Почему выбирают зоомагазин <span>«Природа» </span>?
+        </h1>
+        <p className={styles.subtitle}>
+          «Природа» – крупнейшая сеть зоомагазинов и интернет-магазин с
+          доставкой по всей России. На сайте priroda-zoo.ru представлен широкий
+          выбор зоотоваров, акции и бонусы, а также сервисы по подбору кормов и
+          полезные статьи о домашних животных. Есть приложение для iOS и
+          Android.
+        </p>
+      </div>
+      <div className={styles.faq_list}>
+        {faqData.map((item, index) => (
+          <div
+            key={index}
+            className={`${styles['faq-item']} ${
+              isOpen[index] ? styles.open : ''
+            }`}
+            onClick={() => toggleAnswer(index)}
+          >
+            <div className={styles.question}>
+              <div>{item.question}</div>
+              <div
+                className={isOpen[index] ? styles['arrow-open'] : styles.arrow}
+              >
+                ▼
+              </div>
             </div>
-            <div className={styles.faq_list}>
-                {faqData.map((item, index) => (
-                    <div
-                        key={index}
-                        className={`${styles['faq-item']} ${
-                            isOpen[index] ? styles.open : ''
-                        }`}
-                        onClick={() => toggleAnswer(index)}
-                    >
-                        <div className={styles.question}>
-                            <div>{item.question}</div>
-                            <div
-                                className={
-                                    isOpen[index]
-                                        ? styles['arrow-open']
-                                        : styles.arrow
-                                }
-                            >
-                                ▼
-                            </div>
-                        </div>
-                        <div
-                            className={`${styles.answer} ${
-                                isOpen[index] ? styles['answer-open'] : ''
-                            }`}
-                        >
-                            {item.answer}
-                        </div>
-                    </div>
-                ))}
+            <div
+              className={`${styles.answer} ${
+                isOpen[index] ? styles['answer-open'] : ''
+              }`}
+            >
+              {item.answer}
             </div>
-        </section>
-    )
+          </div>
+        ))}
+      </div>
+    </section>
+  )
 }
 
 export default FAQList
