db4c6984a105f78c6ba4950a97c5c16b09ae21d6
ortima <account.net@.mail.ru>
2024-04-06 20:18:21 +0300

    new

---
+++ .eslintrc.cjs
@@ -0,0 +1,24 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+    commonjs: true,
+    browser: true,
+    es6: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:react-hooks/recommended',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  settings: { react: { version: '18.2' } },
+  plugins: ['react-refresh'],
+  rules: {
+    'no-unused-vars': 'warn',
+    'no-var': 'error',
+    'prefer-const': 'warn',
+  },
+}
